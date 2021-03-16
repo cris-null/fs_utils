66b6f4613c3d3c7b4e81a5368c96034880197108
cris-null <cris@csalazar.net>
2021-03-16 01:19:02 -0500

    Copy single file to single dir

--- src/main/kotlin/FileCopier.kt
+++ src/main/kotlin/FileCopier.kt
@@ -1,5 +1,21 @@
 import java.io.File
 
+/**
+ * @throws IllegalArgumentException If [file] is not a [File], or if
+ * [dir] is not a directory
+ * @throws IllegalArgumentException If either of them don't exist
+ */
+fun copyFileToDir(file: File, dir: File) {
+    if (!file.exists() || !dir.exists()) throw IllegalArgumentException("Argument does not exist")
+    if (file.isDirectory || !dir.isDirectory) throw IllegalArgumentException("Arguments must be a File and a Directory")
+
+    val filename = file.name
+    val destPath = dir.path + "/$filename"
+    val dest = File(destPath)
+
+    file.copyTo(dest)
+}
+
 fun copyFileToAllAdjacentDirs(file: File) {
 
 }
