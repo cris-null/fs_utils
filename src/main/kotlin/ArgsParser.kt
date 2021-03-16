f9c1afa60977c673234cffec89c2d2f31d7903b1
cris-null <cris@csalazar.net>
2021-03-16 01:19:02 -0500

    Copy single file to single dir

--- src/main/kotlin/ArgsParser.kt
+++ src/main/kotlin/ArgsParser.kt
@@ -3,7 +3,10 @@ import java.io.File
 fun parse(args: Array<String>) {
     if (args.isEmpty()) return
 
-    if (args[0] == "info" && args.size == 2) {
+    val command = args[0]
+    if (command == "info" && args.size == 2) {
         printFileInfo(File(args[1]))
+    } else if (command == "copy" && args.size == 3) {
+        copyFileToDir(File(args[1]), File(args[2]))
     }
 }
